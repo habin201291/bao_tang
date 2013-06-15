240734db17f578e7ab90d6b2cbe1d05838997030
vagrant <vagrant@lucid64.hsd1.ca.comcast.net>
2013-06-15 03:41:18 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BaoTang::Application.config.secret_token = '265fdc7fbd27d45334d95b3577c9953f84ed899cd13279c5a7b3e7113eadc2218289228c86dc0f9d2500365cd0f19abb978e8e574af09486c949356043939ae7'
