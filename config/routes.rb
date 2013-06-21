45d17ac584aaa683fc0dff285e101d8dd7cc2a35
Habin2012 <habin201291@gmail.com>
2013-06-21 15:06:24 +0700

    added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 BaoTang::Application.routes.draw do
+  resources :materials
+
+
+  resources :artifacts
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
